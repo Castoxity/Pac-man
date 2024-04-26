5f716824d258a95262d7503a8365b7a085edd516
Castoxity Sama <sameerrajchal@gmail.com>
2024-04-26 12:07:07 +0545

    setup

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
+canvas.height = innerHeight
+
+ class Boundary{
+    constructor({position}){                  
+        // position inside the constructor so u dont gotta remember the series they get used in
+        this.position = position
+        this.width = 40
+        this.height= 40
+    }
+    // 'draw' isnt pre defined, its user's choice of  wording
+    draw(){
+        
+    }
+ }
